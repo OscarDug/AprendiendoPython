57b488c1c99e4be333aa1ad8bab255485f4ef398
OscarDug <ancduglas@gmail.com>
2025-09-07 17:07:14 -0400

    Operadores

--- 1.Introduccion/VariablesConstantes.py
+++ 1.Introduccion/VariablesConstantes.py
@@ -6,3 +6,5 @@ print(cantidadPersonas)
 
 PI = 3.14
 print(PI)
+
+ 
---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,11 @@
+print (5+6)
+print (5-6)
+print (5*6)
+print (5/6)
+print (15*6)
+# levado al 3 cubo
+print(2 ** 3)
+print(75 // 9)
+# modulo devuelve lo que sobro
+print(100 % 75 )
+
